999b3a072b843fb16d85ba69082e89677c3f7bb1
Mahmoud Sakr <ma7mouedsakr@gmail.com>
2021-09-22 22:35:05 +0200

    okay

--- app.py
+++ app.py
@@ -3,9 +3,5 @@ from website import create_app
 app = create_app()
 ENV = "Production"
 # ENV = "Development"
+app.run(debug=False)
 
-if __name__ == '__main__':
-    if ENV == "Production":
-        app.run(debug=False)
-    elif ENV == "Development":
-        app.run(debug=True,port=5000)
